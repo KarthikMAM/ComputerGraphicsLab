3483dfa0c98fb2a3aca2168ece153ad4a1fa53ab
Karthik M A M <karthik_m_a_m@outlook.com>
2015-10-08 19:16:40 +0530

    Update Solar3d.cpp

--- Model Questions/solar3d.cpp
+++ Model Questions/solar3d.cpp
@@ -2,13 +2,14 @@
 
 float matSpecular[] = {1, 1, 1, 1};
 float matShininess[] = {70};
-float lightPos[] = {0, 0, 0, 1};
+float lightPos[] = {30, 30, 30, 1};
 float lightAmb[] = {1, 1, 1, 0};
 
 void init() {
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
+    glEnable(GL_COLOR_MATERIAL);
 
     glShadeModel(GL_SMOOTH);
     glMaterialfv(GL_FRONT, GL_SPECULAR, matSpecular);
@@ -18,32 +19,39 @@ void init() {
     glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmb);
 }
 
-int ro = 2;
+int rotateAngle = 2;
 
 void display() {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(-50, 50, -50, 50, -50, 50);
+    glOrtho(-100, 100, -100, 100, -100, 100);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glutSolidSphere(4, 100, 100);
-    glRotatef(ro, 0, 0, 1);
+    glRotatef(40, 1, 1, 0);
+    glColor3f(1, 1, 0);
+    glutSolidSphere(8, 100, 100);
+    glColor3f(1, 1, 1);
 
-    for(int i=0; i<2; i++) {
-        glTranslatef(0, 20, 0);
-        glRotatef(ro, 1, 1, 0);
+    for(int i=1; i<5; i++) {
+        glutSolidTorus(0.5, i * 20, 100, 100);
+
+        glRotatef(rotateAngle, 0, 0, 1);
+        glTranslatef(0, i * 20, 0);
+        glColor3f(0.2 * i, 0.1 * i, 0.05 * i);
         glutSolidSphere(4, 100, 100);
+        glColor3f(1, 1, 1);
+        glTranslatef(0, -i * 20, 0);
     }
 
     glFlush();
 }
 
 void special(int key, int x, int y) {
-    ro++;
+    rotateAngle++;
     glutPostRedisplay();
 }
 
